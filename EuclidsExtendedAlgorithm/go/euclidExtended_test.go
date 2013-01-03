08e94ae4928cc11e0b27810052ffcea6f342fa14
William Blankenship <william.jblankenship@gmail.com>
2013-01-03 16:26:49 -0600

    Added more test cases. Algorithm should be done and is running correctly.

--- EuclidsExtendedAlgorithm/go/euclidExtended_test.go
+++ EuclidsExtendedAlgorithm/go/euclidExtended_test.go
@@ -15,20 +15,27 @@ var TestCases = []struct{
 	{-1769,551,0,0,0,errors.New(NonPos)},
 	{1769,-551,0,0,0,errors.New(NonPos)},
 	{-1769,-551,0,0,0,errors.New(NonPos)},
+	{120,23,-9,47,1,nil},
+	{-120,23,0,0,0,errors.New(NonPos)},
+	{120,-23,0,0,0,errors.New(NonPos)},
+	{-120,-23,0,0,0,errors.New(NonPos)},
+	{57,46,21,-26,1,nil},
+	{-57,46,0,0,0,errors.New(NonPos)},
+	{57,-46,0,0,0,errors.New(NonPos)},
+	{-57,-46,0,0,0,errors.New(NonPos)},
 }
 
 func TestEuclidsExtendedAlgorithm(t *testing.T) {
 	for i,tt := range TestCases {
 		result1,result2,result3,err:=EuclidExtended(tt.input1,tt.input2)
-		if result1!=tt.output1||result2!=tt.output2||result3!=result3 {
+		if err==nil&&tt.err!=nil {
+			t.Error("Index ",i, " DIDNT RETURN EXPECTED ERROR. Input:",tt.input1," ",tt.input2," Expected Error:",tt.err," Returned Error:",err,"\n")
+		} else if err!=nil&&tt.err==nil {
+			t.Error("Index ",i, " RETURNED UNEXPECTED ERROR. Input:",tt.input1," ",tt.input2," Expected Error:",tt.err," Returned Error:",err,"\n")
+		} else if result1!=tt.output1||result2!=tt.output2||result3!=result3 {
 			t.Error("Index ",i," FAILED. Input:",tt.input1," ",tt.input2," Output: ",result1," ",result2," ",result3," ",err,"\n")
 		} else if  ( err!=nil&&tt.err!=nil ) && ( err.Error()!=tt.err.Error() ) {
-			t.Error("Index ",i," RETURNED WRONG ERROR. Input:",tt.input1," ",tt.input2," Error Returned:",err)
-		} else if err==nil&&tt.err!=nil {
-			t.Error("Index ",i, " DIDNT RETURN EXPECTED ERROR. Input:",tt.input1," ",tt.input2," Expected Error:",tt.err," Returned Error:",err)
-		} else if err!=nil&&tt.err==nil {
-			t.Error("Index ",i, " RETURNED UNEXPECTED ERROR. Input:",tt.input1," ",tt.input2," Expected Error:",tt.err," Returned Error:",err)
+			t.Error("Index ",i," RETURNED WRONG ERROR. Input:",tt.input1," ",tt.input2," Error Returned:",err,"\n")
 		}
 	}
 }
-

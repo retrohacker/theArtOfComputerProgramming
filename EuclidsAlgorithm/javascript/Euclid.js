d66ddc0d031a7beccd2613b61a5b6f16d9f3c496
William Blankenship <william.jblankenship@gmail.com>
2013-01-04 14:20:16 -0600

    Commented functions and used better variable names. Updated error reporting in unit test to be more precise.

--- EuclidsAlgorithm/javascript/Euclid.js
+++ EuclidsAlgorithm/javascript/Euclid.js
@@ -1,10 +1,15 @@
-function Euclid(m,n) {
-	if(m<=0||n<=0){return -1;}
-	var r
+/**
+ * Euclid's algorithm can be used to find the greatest commmon denominator
+ * between to input values. This function will return 0 if either input is
+ * 0 or -1 if either input is negative.
+ */
+function Euclid(input1,input2) {
+	if(input1==0||input2==0){return 0;}
+	if(input1<0||input2<0){return -1;}
 	while(true) {
-		r=m%n;
-		if(r==0) {return n;}
-		m = n;
-		n = r;
+		remainder=input1%input2;
+		if(remainder==0) {return input2;}
+		input1 = input2;
+		input2 = remainder;
 	}
 }
--- EuclidsAlgorithm/javascript/TestEuclid.js
+++ EuclidsAlgorithm/javascript/TestEuclid.js
@@ -1,33 +1,49 @@
+//A value of input/output pairs for a unit test
 var testCases = [
 	[20,100,20],
-	[100,20,20],
+	[100,20,0],
 	[8,15,1],
-	[15,8,1],
+	[15,8,0],
 	[21,133,7],
-	[133,21,7],
+	[133,21,0],
 	[36,4352,4],
-	[4352,36,4]
+	[4352,36,0],
+	[-8,15,-1],
+	[8,-15,-1],
+	[-8,-15,-1],
+	[0,36,0],
+	[4352,0,0],
+	[0,0,0]
 ];
 
+/**
+ * This function will act as a unit test of Euclid's algorithm. It uses a table
+ * driven system for testing values.
+ */
 function TestEuclid() {
 	passed = true;
 	for(i=0;i<testCases.length;i++) {
 		if(!verify(testCases[i],i)) {
 			passed = false;
-			break;
 		}
 	}	
 	if(passed) {
 		document.write("OK (Tests Passed)");
 	} else {
-		document.write("Tests FAILED");
+		document.write("FAIL (One or more tests failed)");
 	}
 }
 
+/**
+ * This function verify the output of Euclid's algorithm with its testcase
+ * data. It accepts two values Number[3] and Number. Number[3] should be in
+ * the form [input1,input2,output]. Number is the test number being run and is
+ * used soely for error reporting.
+ */
 function verify(testCase, caseNum) {
 	result = Euclid(testCase[0],testCase[1])
 	if(result!=testCase[2]) {
-		document.write("Case "+caseNum+" FAILED with Output: "+result+"<br/>");
+		document.write("Case "+caseNum+" FAILED with Output: "+result+" Expected: "+testCase[2]+"<br/>");
 		return false;
 	}
 	return true;

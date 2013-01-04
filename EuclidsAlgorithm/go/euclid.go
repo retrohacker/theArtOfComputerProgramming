fda72780ceb59a31842774c90fb61c7271136cb2
William Blankenship <william.jblankenship@gmail.com>
2013-01-04 13:50:10 -0600

    Used better variable names in euclid.go

--- EuclidsAlgorithm/go/euclid.go
+++ EuclidsAlgorithm/go/euclid.go
@@ -5,15 +5,15 @@
 package euclid
 
 // Function euclid finds the greates common denominator between two unsigned
-// integers m and n. If either value is 0 the result will be 0.
-func euclid(m,n uint) (uint) {
-	var r uint
-	if(m==0||n==0) {return 0}
+// If either value is 0 the result will be 0.
+func euclid(input1,input2 uint) (uint) {
+	var remainder uint
+	if(input1==0||input2==0) {return 0}
 	for {
-		r = m%n
-		if(r==0) {break}
-		m = n
-		n = r
+		remainder = input1%input2
+		if(remainder==0) {break}
+		input1 = input2
+		input2 = remainder
 	}
-	return n
+	return input2
 }
